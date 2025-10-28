bd29e63862cfd8591c08b4701ac3cf48db961630
mdasifalijihat <mdasifalijihat@gmail.com>
2025-10-28 08:24:26 +0600

    added dotenv mongos express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('asif')
+import express from 'express';
+import 'dotenv/config';
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port:${PORT}`);
+});
