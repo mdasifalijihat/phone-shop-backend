b80457f21bce1d66f4b8f67c31b16bc8fc65ae0a
mdasifalijihat <mdasifalijihat@gmail.com>
2025-10-28 09:11:04 +0600

    added controllers

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+import { User } from "../models/userModel";
+
+
+
+
+export const register = async (req, res) => {
+    try {
+        const { firstName, lastName, email, password } = req.body;
+        if (!firstName || !lastName || !email || !password) {
+            return res.status(400).json({ success: false, message: "All fields are required" });
+        }
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(409).json({ success: false, message: "User already exists" });
+        }
+        const newUser = await User.create({
+            firstName, lastName, email, password
+        });
+        await newUser.save();
+        return res.status(201).json({ success: true, message: "User registered successfully", user: newUser }); 
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server Error", error: error.message });
+     }
+}
