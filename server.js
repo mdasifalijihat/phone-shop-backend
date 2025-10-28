b91d427212ee30e52688751c1bed2223cbf9e18c
mdasifalijihat <mdasifalijihat@gmail.com>
2025-10-28 08:55:39 +0600

    added user models

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
+import connectDB from './database/db.js';
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { 
+    connectDB();
     console.log(`Server is running on port:${PORT}`);
 });
