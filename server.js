11cb6fcfea31247775cdc25ecc6dd68c590c31bb
mdasifalijihat <mdasifalijihat@gmail.com>
2025-11-02 10:57:17 +0600

    added cors

--- server.js
+++ server.js
@@ -2,20 +2,29 @@ import express from 'express';
 import 'dotenv/config';
 import connectDB from './database/db.js';
 import userRoute from './routers/userRoute.js';
+import cors from 'cors';
+
 
  
-const app = express();
+const app = express();  
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173', // Adjust according to your frontend's address
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 
 app.use('/api/v1/user', userRoute);  
 
-//http://localhost:8000/api/v1/user/register"
+
 
 
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on port:${PORT}`);
 });
+
+
